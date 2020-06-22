8f6e49695a4f90d1d63f70bbf2fe034caf3076cd
Drew Hart <drew.ellison.hart@gmail.com>
2020-06-22 11:36:30 -0700

    Small tweak to archive_MODIS_background.py

--- archive_MODIS_background.py
+++ archive_MODIS_background.py
@@ -4,12 +4,12 @@ import os
 home_dir = '/home/drew/'
 txt_file = os.path.join(home_dir, 'MODIS_daily_img/today.txt')
 img_file = os.path.join(home_dir, 'Pictures/MODIS_daily.jpg')
-print(txt_file)
 with open(txt_file, 'r') as f:
     date = f.readlines()[0].strip()
 
 # archive image and text
 archive_dir = os.path.join(home_dir, 'MODIS_daily_img/archive', date)
+print('SAVING TODAY'S IMAGE AND TEXT TO DIRECTORY: %s' % archive_dir)
 os.mkdir(archive_dir)
 txt_cp_cmd = 'cp %s %s' % (txt_file, os.path.join(archive_dir,
                                                   'description.txt'))
